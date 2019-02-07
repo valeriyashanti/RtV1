2649e6fab51ffe4ab5893c8252d372d68a482f61
Валерия <35383612+valeriyashanti@users.noreply.github.com>
2019-02-07 16:32:45 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gkessler <gkessler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 17:13:26 by gkessler          #+#    #+#             */
-/*   Updated: 2019/02/07 16:09:25 by gkessler         ###   ########.fr       */
+/*   Updated: 2019/02/07 16:32:28 by gkessler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,10 @@ int		get_light(t_obj obj, int i, int j, t_rt rt)
 	c.y = (i * 1.0 - 300.0) / 600.0;
 	c.z = 1.0;
 
+	t_vec3 null;
+	null.x = 0.0;
+	null.y = 0.0;
+	null.z = 0.0;
 	t_vec3 d; //направление вектора луча
 	d = vec_minus(c, rt.cam);
 
@@ -75,17 +79,16 @@ int		get_light(t_obj obj, int i, int j, t_rt rt)
 	// printf("%lf %lf %lf\n", rt.cam.x, rt.cam.y, rt.cam.z);
 
 	t_vec3 p; //точка
-	t_vec3 null;
-	null.x = 0.0;
-	null.y = 0.0;
-	null.z = 0.0;
 	p = vec_plus(null, vec_mul(d, rt.res));
+	// printf("P - %lf %lf %lf\n", p.x, p.y, p.z);
 
 	t_vec3 n; //нормаль
 	n = vec_div(vec_minus(p, obj.dot), vec_mod_div(p, obj.dot));
+	// printf("N - %lf %lf %lf\n", n.x, n.y, n.z);
 
 	t_vec3 l; //вектор луча света
 	l = vec_minus(p, rt.light.dot);
+	// printf("L - %lf %lf %lf\n", l.x, l.y, l.z);
 
 	double sc; // cкаляр
 	sc = vec_sc(n, l);
@@ -122,24 +125,26 @@ void	rtv1(t_rt *rt)
 	s.color = 0x0000ff;
 	//s.oc = vec_plus(s.dot, vec_mul(rt->cam, -1));
 
-	/* t_obj s1;
-	s1.dot.x = 1;
-	s1.dot.y = 1;
-	s1.dot.z = 20;
-	s1.radius = 1;
+	t_obj s1;
+	s1.dot.x = -2.0;
+	s1.dot.y = -2.0;
+	s1.dot.z = 10.0;
+	s1.radius = 1.0;
 	s1.color = 0xff0000;
 
 	t_obj s2;
-	s2.dot.x = 0;
-	s2.dot.y = 0;
-	s2.dot.z = 30;
-	s2.radius = 10;
-	s2.color = 0x00ff00; */
+	s2.dot.x = 3.0;
+	s2.dot.y = 3.0;
+	s2.dot.z = 10.0;
+	s2.radius = 1.0;
+	s2.color = 0x00ff00;
 
 	rt->light.dot.x = 0.0;
 	rt->light.dot.y = 0.0;
 	rt->light.dot.z = 5.0;
-	rt->light.inten = 0.2;
+	rt->light.inten = 0.5;
+
+	t_obj o;
 
 	i = 0;
 	while (i < W_H)
@@ -150,33 +155,37 @@ void	rtv1(t_rt *rt)
 			rt->res = 1000000;
 			double col;
 			k = 0;
-			while (k < 1)
+			while (k < 3)
 			{
 				if (k == 0)
 				{
 					res0 = ray(j, i, &s, rt);
 					col = s.color;
+					o = s;
 				}
-				// if (k == 1)
-				// {
-				// 	res0 = ray(j, i, &s1, rt);
-				// 	col = s1.color;
-				// }
-				// if (k == 2)
-				// {
-				// 	res0 = ray(j, i, &s2, rt);
-				// 	col = s2.color;	
-				// }
+				 if (k == 1)
+				 {
+				 	res0 = ray(j, i, &s1, rt);
+				 	col = s1.color;
+					o = s1;
+				 }
+				 if (k == 2)
+				 {
+					res0 = ray(j, i, &s2, rt);
+					col = s2.color;
+					o = s2;
+				}
 				if (res0 > 1.0 && res0 < rt->res)
 				{
 					rt->res = res0;
 					rt->color = col;
+					rt->obj = o;
 				}
 				k++;
 			}
 			if (rt->res > 1.0 && rt->res < 1000000.0)
 			{
-				rt->color = get_light(s, i, j, *rt);
+				rt->color = get_light(rt->obj, i, j, *rt);
 				mlx_pixel_put(rt->mlx_ptr, rt->win_ptr, j, i, rt->color);
 			}
 			else
